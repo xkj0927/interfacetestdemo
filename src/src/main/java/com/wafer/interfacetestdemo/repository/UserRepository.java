228f25db2c34f2c73f3e083c77c49dc7baabff31
liupc <itliupc@163.com>
2017-08-18 13:42:08 +0800

    添加用户信息维护后台代码

--- src/src/main/java/com/wafer/interfacetestdemo/repository/UserRepository.java
+++ src/src/main/java/com/wafer/interfacetestdemo/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.wafer.interfacetestdemo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.wafer.interfacetestdemo.domain.User;
 import com.wafer.interfacetestdemo.repository.base.BaseRepository;
@@ -12,6 +14,13 @@ public interface UserRepository extends BaseRepository<User, Long> {
   
   User findByUserName(String userName);
 
-  @Query(value = "Select new com.wafer.interfacetestdemo.vo.UserVo (u.userId, u.userName, u.email, u.latestLoginTime) from User u where u.email != 'admins'")
+  @Modifying
+  @Query(value = "Update User u set u.status = 1 where u.userId = :userId")
+  void updateUserStatusByUserId(@Param("userId") long userId);
+
+  @Query(value = "Select new com.wafer.interfacetestdemo.vo.UserVo (u.userId, u.userName, u.email, u.latestLoginTime) from User u where u.status = 0 and u.email != 'admin'")
   List<UserVo> getUserVoList();
+
+  @Query(value = "from User u where u.email = :email")
+  User getUserbyEmail(@Param("email") String email);
 }
---
+++ src/src/main/java/com/wafer/interfacetestdemo/vo/LoginUserVo.java
@@ -0,0 +1,35 @@
+package com.wafer.interfacetestdemo.vo;
+
+import com.wafer.interfacetestdemo.domain.User;
+
+public class LoginUserVo {
+  private String token;
+  private User user;
+  
+  public String getToken() {
+    return token;
+  }
+  
+  public void setToken(String token) {
+    this.token = token;
+  }
+  
+  public User getUser() {
+    return user;
+  }
+  
+  public void setUser(User user) {
+    this.user = user;
+  }
+  
+  public LoginUserVo() {
+    super();
+  } 
+  
+  public LoginUserVo(String token, User user) {
+    super();
+    this.token = token;
+    this.user = user;
+  }  
+  
+}
